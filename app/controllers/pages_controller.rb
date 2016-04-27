3b058ae2a9adaefc8f5d22d572971c7019bd1603
Mark Blum <m_blum23@yahoo.com>
2016-04-27 18:24:14 +0000

    Styled the home page and profile page to give it that extra flare!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -23,11 +23,15 @@ class PagesController < ApplicationController
     
     @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
     @newPost = Post.new
+    
+    @toFollow = User.all.last(5)
   end
   
   # back-end code for pages/explore
   def explore
     @posts = Post.all
+    @newPost = Post.new
+        @toFollow = User.all.last(5)
   end
   
 end

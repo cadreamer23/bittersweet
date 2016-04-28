48dbf23106f4c2f824053adc63ee8d95984d98c8
Mark Blum <m_blum23@yahoo.com>
2016-04-28 15:54:47 +0000

    I implemented follower and following relationships, setted up the routes and database for relationships. Now you can follow the master of disaster!

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   
   devise_for :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
   resources :posts
-  #Define Root URL
+  # Define Root URL
   root 'pages#index'
   
   # Define Routes for Pages
